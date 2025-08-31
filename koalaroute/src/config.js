42b4fc2ffc76715cc29944e30e241bb9afd2e7f3
Birajsitaula <birajsitaula7@gmail.com>
2025-08-31 18:52:41 +0545

    hhh

--- koalaroute/src/config.js
+++ koalaroute/src/config.js
@@ -1,6 +1,4 @@
 // export const BASE_URL = "http://localhost:5000/api";
 
 export const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_BASE_URL
-    : import.meta.env.VITE_BASE_URL_PROD;
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"; // Use relative path for production
