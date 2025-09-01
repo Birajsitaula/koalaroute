f490e10e6e4c2810abdd89e342aab0ccaf69a83c
Birajsitaula <birajsitaula7@gmail.com>
2025-09-01 08:40:13 +0545

    new

--- koalaroute/src/config.js
+++ koalaroute/src/config.js
@@ -3,4 +3,4 @@
 export const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:8888/.netlify/functions/api"
-    : "https://your-netlify-site.netlify.app/.netlify/functions/api";
+    : "https://koalaroutes.netlify.app//.netlify/functions/api";
