752ac518517901ecb0957514e2c782d5102394f7
Birajsitaula <birajsitaula7@gmail.com>
2025-09-02 12:32:05 +0545

    updated

--- koalaroute/src/config.js
+++ koalaroute/src/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = import.meta.env.DEV
-  ? "http://localhost:8888/.netlify/functions/api"
-  : import.meta.env.VITE_BASE_URL;
+// export const BASE_URL = import.meta.env.DEV
+//   ? "http://localhost:8888/.netlify/functions/api"
+//   : import.meta.env.VITE_BASE_URL;
+
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
